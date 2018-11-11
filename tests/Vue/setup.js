567d9aed3b59a6f2d58fc00d90bf95cd1bc656f0
HOME_신형준 <ssa4141@naver.com>
2018-11-11 21:29:48 +0900

    setup

--- tests/Vue/setup.js
+++ tests/Vue/setup.js
@@ -1,4 +1,21 @@
 require('jsdom-global')();
 
+let see = (text, selector) => {
+    let wrap = selector ? wrapper.find(selector) : wrapper;
+
+    expect(wrap.html()).toContain(text);
+};
+
+let type = (selector, text) => {
+    let node = wrapper.find(selector);
+
+    node.element.value = text;
+    node.trigger('input');
+};
+
+let click = (selector) => {
+    wrapper.find(selector).trigger('click');
+};
+
 // window.Date = Date 추가 안하면 오류남. 특정 버전 이상에서 나는 오류라고 함.
 window.Date = Date;
